b7bd06c8897828c730e2526f27dc8c0cdfd11031
Alexandre Passant <apassant@gmail.com>
2012-01-28 17:48:29 +0100

    /song

--- mixture/urls.py
+++ mixture/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^image/', 'image.views.index'),
+    url(r'^song/', 'song.views.index'),
 )
