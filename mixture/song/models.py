f1ba4fbe515f18aae0bec09b3cfa706f1e53bc1f
Alexandre Passant <apassant@gmail.com>
2012-01-28 22:07:12 +0100

    Filter seevl-matching artists

--- mixture/song/models.py
+++ mixture/song/models.py
@@ -6,6 +6,9 @@ from musixmatch import track as TRACK
 from musixmatch import artist as ARTIST
 from musixmatch import tracking as TRACKING
 
+from seevl.seevl import SeevlEntitySearch
+from django.conf import settings
+
 # Create your models here.
 
 class MusixMatch(object):
@@ -13,6 +16,11 @@ class MusixMatch(object):
     def __init__(self, query):
         self.query = query
         self.apikey = 'a4bdc6b3fa75aea2fef27dde5da26dc3'
-        self.tracks = TRACK.search(q=self.query, apikey=self.apikey, page_size=100)
-
-
+        self.tracks = self.filterTracks(TRACK.search(q=self.query, apikey=self.apikey, page_size=10))
+        
+    def filterTracks(self, tracks):
+        """Get only tracks with artist matching in seevl"""
+        return filter(lambda x: 
+            len(SeevlEntitySearch({'prefLabel' : x.artist_name}, seevl_app_id = settings.SEEVL_APP_ID, seevl_app_key = settings.SEEVL_APP_KEY).run()),
+            tracks)
+        
