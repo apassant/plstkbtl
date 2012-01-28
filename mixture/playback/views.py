7c3074b761ef3c7502460b4afe698982305f356c
Ian McKellar <ian@mckellar.org>
2012-01-29 00:06:07 +0100

    testing

--- mixture/playback/views.py
+++ mixture/playback/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponse
 from django.conf import settings
 from django.shortcuts import render_to_response
 
-import logging
-logging.info('hello')
-from mixture.seevl import SeevlEntitySearch
+from mixture.seevl.seevl import SeevlEntitySearch
 from rdio import Rdio
 rdio = Rdio((settings.RDIO_KEY, settings.RDIO_SECRET))
 
