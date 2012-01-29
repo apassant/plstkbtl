f2aa10b50890468b10c5577edcaea529f28938f5
Alexandre Passant <apassant@gmail.com>
2012-01-29 12:31:16 +0100

    Removed old seevl folder

--- mixture/seevl/ex.py
+++
@@ -1,5 +0,0 @@
-from seevl import SeevlEntitySearch
-for s in SeevlEntitySearch({'prefLabel' : 'the beatles'}).run():
-    print "%s is an artist from:" %s.prefLabel
-    for o in s.facts['origin']:
-        print "- %s" %o['prefLabel']
