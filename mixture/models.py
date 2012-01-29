2f832ca5818ec0bd5f452e380d1eb821f86b7226
Alexandre Passant <apassant@gmail.com>
2012-01-29 13:12:36 +0100

    Cleaned MxM class and API key management

--- mixture/models.py
+++ mixture/models.py
@@ -1,21 +1,21 @@
-from django.db import models
+from django.conf import settings
 
 from dependencies.musixmatch.musixmatch import track as TRACK
 from dependencies.seevl.seevl.seevl import SeevlEntitySearch
 
-from django.conf import settings
-
-# Create your models here.
 class MusixMatch(object):
-
+    """Integraion with MxM API"""
+    
     def __init__(self):
-        self.apikey = 'a4bdc6b3fa75aea2fef27dde5da26dc3'
+        """Setup API key"""
+        self.apikey = settings.MXM_API_KEY
         
     def getTracks(self, query):
+        """Get tracks matching a query term"""
         return self.filterTracks(TRACK.search(q=query, apikey=self.apikey, page_size=10))
         
     def filterTracks(self, tracks):
-        """Get only tracks with artist matching in seevl"""
+        """Filter to get only tracks with artist matching in seevl"""
         return filter(lambda x: 
             len(SeevlEntitySearch({
                 'prefLabel' : x.artist_name.encode('utf-8')
@@ -23,6 +23,7 @@ class MusixMatch(object):
             tracks)
     
     def getTrack(self, track):
+        """Get track infos"""
         track = TRACK.Track(track)
         return {
             'artist' : track.artist_name,
